689b611533a58d721298c9fd3bd2d8a3fbaeffe3
¨jonathansansok¨ <jonasans2@live.com.ar>
2023-07-25 08:31:09 -0300

    js

--- assets/js/validaciones.js
+++ assets/js/validaciones.js
@@ -1,10 +1,13 @@
-
+var expr = /^ [a-zA-Zo-9_\.\-]+@[a-zA-zo-9\-]+\. [a-zA-Zo-9\-\.]+$/;
+  
  $(document).ready(function() {
+
     $('#form-catgs').submit(function(event){
         event.preventDefault();
         $('#form-catgs input[type="text"], #form-catgs textarea').each(function(){
+
           if($(this).val() === ''){
-            alert('Please enter a value, cannot be empty - Developed by John Sansó');
+            alert('Debe completar la seccion ' +  $(this).attr('name') + ' - Developed by John Sansó');
             $(this).focus();
             return false;
           }   
@@ -12,7 +15,9 @@
     })
     //////////////////////
     $('#form-prods').submit(function(event){
+
         event.preventDefault();
+
         $('#form-prods input[type="text"], #form-prods textarea').each(function(){
           if($(this).val() === ''){
             alert('Debe completar la seccion ' +  $(this).attr('name') + ' - Developed by John Sansó');
